b18ff4760300a0fc8be1d203a0ae1a97f8078056
KlaudiaPolan <polan.szlachta@gmail.com>
2017-07-10 23:45:56 +0200

    added link to my github - To do list

--- js/about.js
+++
@@ -1,7 +0,0 @@
-/*var height=$('#about_img').height();
-$('.about1').css('padding-top', ((300-height))+'px');
-
-$(window).resize(function(){
-  height=$('#about_img').height();
-  $('.about1').css('padding-top', ((300-height))+'px');
-});*/
