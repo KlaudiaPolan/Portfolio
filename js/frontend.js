62ba89247edf917ffab1557dc3cd0aacb62994cc
KlaudiaPolan <polan.szlachta@gmail.com>
2017-07-25 11:58:50 +0200

    fixed animations in the gallery

--- js/frontend.js
+++ js/frontend.js
@@ -1,20 +1,31 @@
+
+
 $('.sitOnCherWeb').mouseover(function() {
+	if($(window).width()>776){
   $('.textSitOnCherWeb').css('margin-left', '0');
+}
 
 });
 $('.sitOnCherWeb').mouseleave(function() {
+	if($(window).width()>776){
   $('.textSitOnCherWeb').css('margin-left', '-50%');
+}
 
 });
 
 $('.toDoList').mouseover(function() {
+	if($(window).width()>776){
   $('.textToDoList').css('margin-left', '0');
+}
 
 });
 $('.toDoList').mouseleave(function() {
+	if($(window).width()>776){
   $('.textToDoList').css('margin-left', '-50%');
+}
 
 });
+
 
 var positionButton = ($('.textToDoList').width()) - 140;
 $('.frontEndButton2').css('left', (positionButton / 2) + 'px');
